fd821302da4b8ee076f6fc86c97241a8ab4b75cf
vidhya <vidhya93soni@gmail.com>
2024-09-10 01:12:44 +0530

    third commit

---
+++ LECTURE 2CP/basics.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x=10;
+    int y=5;
+    int z=4;
+    int a[]={1,2,3};
+    int b[]={1,2,4};
+    int c(40);
+    int xy = 20.6;
+    cout<<xy<<endl;
+}
